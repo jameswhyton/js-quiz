d6e1d5cdde9e9b2a43d1732b4bf34260b4ac3a04
James Whyton <james.whyton@dpdgroup.co.uk>
2021-05-27 12:00:31 +0100

    Question 4 Answer

--- src/questions/question4.js
+++ src/questions/question4.js
@@ -31,5 +31,53 @@ exports.questionFour = () => {
 
 class HighScores {
   //code here
+  constructor(scoresInput) {
+    this._scoresInput = scoresInput;
+  }
+  get scores() {
+    return this._scoresInput;
+  }
+
+  get latest() {
+    const lastScore = this._scoresInput.length - 1;
+    return this._scoresInput[lastScore];
+  }
+
+  get personalBest() {
+    const scoresSorted = this._scoresInput.sort((a, b) => b - a);
+    return scoresSorted[0];
+  }
+
+  get personalTopThree() {
+    const scoresSorted = this._scoresInput.sort((a, b) => b - a);
+    return scoresSorted.slice(0, 3);
+  }
 
 }
+
+const scoresInput = [30, 50, 20, 70];
+const scores = new HighScores(scoresInput).scores; //expected result: [30, 50, 20, 70]
+console.log(scores);
+
+const latestInput = [100, 0, 90, 30];
+const latest = new HighScores(latestInput).latest; //expected result: 30
+console.log(latest);
+
+const pbInput = [40, 100, 70];
+const pb = new HighScores(pbInput).personalBest; //expected result: 100
+console.log(pb);
+
+const top3Input = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70];
+const top3Result1 = new HighScores(top3Input).personalTopThree; //expected result: [ 100, 90, 70 ]
+console.log(top3Result1);
+
+const top3Input2 = [40, 20, 40, 30];
+const top3Result2 = new HighScores(top3Input2).personalTopThree; //expected result: [40, 40, 30]
+console.log(top3Result2);
+
+
+//my tests
+
+const scoresInput2 = [123, 456, 789, 0];
+const scores2 = new HighScores(scoresInput2).personalTopThree;
+console.log(scores2);
