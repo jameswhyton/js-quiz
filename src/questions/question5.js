290eb50002a1fab542943b7d4fd4822438493576
James Whyton <james.whyton@dpdgroup.co.uk>
2021-05-27 15:59:04 +0100

    Question 5 partially done - not complete

--- src/questions/question5.js
+++ src/questions/question5.js
@@ -12,5 +12,29 @@ const answerFive = () => {
   const url = "https://europe-west2-dpduk-t-insight-l1.cloudfunctions.net/employees";
 
   //code here
+  axios.interceptors.response.use((response) => {
+    // do something with the response data
+    const today = new Date().toISOString().slice(0, 10).split("-"); // returns todays date as YYYY-MM-DD (in array)
+    const fiftyYrsAgo = Number(today[0] - 50 + today.slice(1, 3).join("")); // returns the date fifty years ago as a number
+    const employeeDob = Number(response.data.dateOfBirth);
 
+
+    return response;
+  }, error => {
+    // handle the response error
+    return Promise.reject(error);
+  });
+
+
+  axios.get(url)
+    .then(response => { console.log(response); })
 }
+
+
+console.log(answerFive());
+
+//Work out the date 50 years ago today.
+const today = new Date().toISOString().slice(0, 10).split("-"); // returns todays date as YYYY-MM-DD (in array)
+const combinedDate = Number(today[0] - 50 + today.slice(1, 3).join(""));
+
+//console.log(combinedDate);
