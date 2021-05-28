90761a99ff864abd1d4898c5f33a4891b9d546cb
James Whyton <james.whyton@dpdgroup.co.uk>
2021-05-28 11:39:35 +0100

    Question 5 Answer

--- src/questions/question5.js
+++ src/questions/question5.js
@@ -8,33 +8,53 @@ exports.questionFive = () => {
   console.log(answerFive());
 }
 
-const answerFive = () => {
+const answerFive = () => { //Answer without async await function
   const url = "https://europe-west2-dpduk-t-insight-l1.cloudfunctions.net/employees";
-
+  //Work out the date 50 years ago today.
+  const today = new Date().toISOString().slice(0, 10).split("-"); // returns todays date as YYYY-MM-DD (in array)
+  const combinedDate = Number(today[0] - 50 + today.slice(1, 3).join("")); // formats date as a number eg 20210528
   //code here
-  axios.interceptors.response.use((response) => {
-    // do something with the response data
-    const today = new Date().toISOString().slice(0, 10).split("-"); // returns todays date as YYYY-MM-DD (in array)
-    const fiftyYrsAgo = Number(today[0] - 50 + today.slice(1, 3).join("")); // returns the date fifty years ago as a number
-    const employeeDob = Number(response.data.dateOfBirth);
-
-
-    return response;
-  }, error => {
-    // handle the response error
-    return Promise.reject(error);
-  });
-
-
   axios.get(url)
-    .then(response => { console.log(response); })
+    .then(response => {
+      const employeeDob = response.data.map(e => Number(e.dateOfBirth.split("-").join(""))); //get the employees date of birth formatted to a number
+      const over50Index = [] // variable to store indexes that meet below condition
+      employeeDob.forEach((e, i) => {
+        if (e <= combinedDate) {
+          over50Index.push(i);
+        }
+      });
+      const over50Array = response.data.filter((el, i) => over50Index.some(j => i === j));
+      console.log(over50Array);
+    })
+
+
+}
+
+const answerFiveAsync = async () => { //answer with Async Await
+  try {
+    const url = "https://europe-west2-dpduk-t-insight-l1.cloudfunctions.net/employees";
+    const response = await axios.get(url);
+
+    const today = new Date().toISOString().slice(0, 10).split("-"); // returns todays date as YYYY-MM-DD (in array)
+    const combinedDate = Number(today[0] - 50 + today.slice(1, 3).join("")); // formats date as a number eg 20210528
+
+    const employeeDob = response.data.map(e => Number(e.dateOfBirth.split("-").join(""))); //get the employees date of birth formatted to a number
+    const over50Index = [] // variable to store indexes that meet below condition
+    employeeDob.forEach((e, i) => {
+      if (e <= combinedDate) {
+        over50Index.push(i);
+      }
+    });
+    const over50Array = response.data.filter((el, i) => over50Index.some(j => i === j));
+    console.log(over50Array);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 
-console.log(answerFive());
+console.log(answerFiveAsync());
 
-//Work out the date 50 years ago today.
-const today = new Date().toISOString().slice(0, 10).split("-"); // returns todays date as YYYY-MM-DD (in array)
-const combinedDate = Number(today[0] - 50 + today.slice(1, 3).join(""));
+
 
 //console.log(combinedDate);
